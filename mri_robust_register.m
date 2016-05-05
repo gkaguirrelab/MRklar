ca094c02f583f0bcf90fab40df7712986ddb5c68
bock42 <abock@mac-pro.uphs.upenn.edu>
2016-05-05 18:45:01 -0400

    mri_robust_register - header updates, remove tmp files

--- mri_robust_register.m
+++ mri_robust_register.m
@@ -1,5 +1,16 @@
 function mri_robust_register(inVol,outVol,outDir)
 
+% Motion corrects a 4D volume using Freesurfer's 'mri_robust_register'
+%
+%   Usage:
+%   mri_robust_register(inVol,outVol,outDir)
+%
+%   Based on:
+%   Highly Accurate Inverse Consistent Registration: A Robust Approach
+%   M. Reuter, H.D. Rosas, B. Fischl.
+%   NeuroImage 53(4), pp. 1181-1196, 2010.
+%
+%   Written by Andrew S Bock May 2016
 
 %% move to output directory
 cd(outDir);
@@ -11,19 +22,21 @@ progBar = ProgressBar(length(inVols),'mri_robust_registering...');
 for i = 1:length(inVols)
     inFile = fullfile(outDir,inVols{i});
     dstFile = fullfile(outDir,inVols{1}); % register to first volume
-    outFile = fullfile(outDir,[num2str(i) '.nii.gz']);
+    outFile = fullfile(outDir,['tmp_' num2str(i) '.nii.gz']);
     [~,~] = system(['mri_robust_register --mov ' inFile ...
         ' --dst ' dstFile ' --lta ' num2str(i) '.lta --satit --mapmov ' ...
         outFile]);
     progBar(i);
 end
+system(['rm ' fullfile(outDir,'split_f0*')]); % remove split volumes
 %% Merge into output 4D volume
 commandc = ['fslmerge -t ' outVol];
 for i = 1:length(inVols)
-    outFile = fullfile(outDir,[num2str(i) '.nii.gz']);
+    outFile = fullfile(outDir,['tmp_' num2str(i) '.nii.gz']);
     commandc = [commandc ' ' outFile];
 end
 system(commandc);
+system(['rm ' fullfile(outDir,'tmp_*nii.gz')]); % remove tmp volumes
 %% Convert .lta files to translations and rotations
 clear x y z pitch yaw roll
 ltaFiles = listdir(fullfile(outDir,'*.lta'),'files');
