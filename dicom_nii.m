7bcbd3e9cc047abec97673e5a1d71d976158f011
bock42 <abock@mac-pro.uphs.upenn.edu>
2016-08-24 14:03:27 -0400

    dicom_nii - added option to use 'dcm2nii' to convert dicoms

--- dicom_nii.m
+++ dicom_nii.m
@@ -1,20 +1,37 @@
-function [] = dicom_nii(dicomDir,outputDir,outputFile)
+function dicom_nii(dicomDir,outDir,outFile,useMRIcron)
 %%
 %
-%   Usage: 
-%   dicom_nii(dicomDir,outputDir,outputFile)
+%   Usage:
+%   dicom_nii(dicomDir,outDir,outFile,useMRIcron)
 %
 % Inputs:
-%   -dicomDir = path to dicom directory (assumes single series)
-%       -if directory has multiple serires, use dicom_sort.m first.
-%   -outputDir = path to output directory for nifti file
-%   -outputFile = name of output file, e.g. f.nii.gz
+%   dicomDir    - path to dicom directory (assumes single series)
+%               - if directory has multiple serires, use dicom_sort.m first.
+%   outDir      - path to output directory for nifti file
+%   outFile     - name of output file, e.g. f.nii.gz
+%   useMRIcron  - 0 = mri_convert (Freesurfer); 1 = dcm2nii (MRIcron)
 %
 % Written by Andrew S Bock Feb 2014
+% Updated by Andrew S Bock Aug 2016 to add the 'useMRIcron' option
 
+%% Set defaults
+if ~exist('useMRIcron','var')
+    useMRIcron = 0;
+end
 fprintf('\nConverting dicoms to nifti\n');
 fprintf(['Input ' dicomDir '\n']);
-fprintf(['Output ' fullfile(outputDir,outputFile) '\n']);
+fprintf(['Output ' fullfile(outDir,outFile) '\n']);
 dicomList = listdir(dicomDir,'files');
-system(['mri_convert -odt float ' fullfile(dicomDir,dicomList{end}) ...
-    ' ' fullfile(outputDir,outputFile)]);
+%% Choose function to convert dicoms to nifti
+if useMRIcron
+    system(['dcm2nii -d n -p n -o ' dicomDir ' ' dicomDir]);
+    created_file = listdir(fullfile(dicomDir,'s0*.nii.gz'),'files');
+    if ~isempty(created_file)
+        movefile(fullfile(dicomDir,created_file{1}),fullfile(outDir,outFile));
+    else
+        disp('dcm2nii failed to convert');
+    end
+else
+    system(['mri_convert -odt float ' fullfile(dicomDir,dicomList{end}) ...
+        ' ' fullfile(outDir,outFile)]);
+end
