f1f9b490a34802e5ab22dc481f8d2816147ec32e
bock42 <abock@mac-pro.uphs.upenn.edu>
2016-10-04 15:57:43 -0400

    [DOC] motion_slice_correction - header updates

--- motion_slice_correction.m
+++ motion_slice_correction.m
@@ -5,19 +5,17 @@ function motion_slice_correction(params,runNum)
 %   Usage:
 %       motion_slice_correction(params,runNum)
 %
-%   Defaults:{runNum}
-%       params.despike                 = 1; % params.despike data
-%       params.slicetiming             = 1; % do slice timing correction
-%       params.runNums                 = 1:length(d); % all runs in session directory
-%       params.refvol                  = 1; % reference volume = 1st TR
+%   Required:
+%       params.sessionDir       = '/path/to/session/directory'
+%
+%   Defaults:
+%       params.despike          = 1; % params.despike data
+%       params.slicetiming      = 1; % do slice timing correction
+%       params.refvol           = 1; % reference volume = 1st TR
 %
 %   Written by Andrew S Bock June 2016
 
 %% Set default parameters
-
-% Find bold run directories
-d = find_bold(params.sessionDir);
-
 if ~isfield(params,'despike')
     params.despike              = 1; % params.despike data
 end
@@ -27,6 +25,8 @@ end
 if ~isfield(params,'refvol')
     params.refvol               = 1; % reference volume = 1st TR
 end
+% Find bold run directories
+d = find_bold(params.sessionDir);
 %% Remove spikes
 if params.despike
     remove_spikes(fullfile(params.sessionDir,d{runNum},'raw_f.nii.gz'),...
@@ -35,38 +35,38 @@ end
 %% Slice timing correction
 if params.slicetiming
     if params.despike
-        inFile              = fullfile(params.sessionDir,d{runNum},'despike_f.nii.gz');
+        inFile                  = fullfile(params.sessionDir,d{runNum},'despike_f.nii.gz');
     else
-        inFile              = fullfile(params.sessionDir,d{runNum},'raw_f.nii.gz');
+        inFile                  = fullfile(params.sessionDir,d{runNum},'raw_f.nii.gz');
     end
-    outFile                 = fullfile(params.sessionDir,d{runNum},'f.nii.gz');
-    timingFile              = fullfile(params.sessionDir,d{runNum},'slicetiming');
+    outFile                     = fullfile(params.sessionDir,d{runNum},'f.nii.gz');
+    timingFile                  = fullfile(params.sessionDir,d{runNum},'slicetiming');
     slice_timing_correction(inFile,outFile,timingFile);
 end
 %% Run motion correction
 if params.slicetiming
-    mcParams.mcFile           = fullfile(params.sessionDir,d{runNum},'f.nii.gz');
-    mcParams.regFile          = fullfile(params.sessionDir,d{1},'f.nii.gz');
+    mcParams.mcFile             = fullfile(params.sessionDir,d{runNum},'f.nii.gz');
+    mcParams.regFile            = fullfile(params.sessionDir,d{1},'f.nii.gz');
 elseif params.despike
-    mcParams.mcFile           = fullfile(params.sessionDir,d{runNum},'despike_f.nii.gz');
-    mcParams.regFile          = fullfile(params.sessionDir,d{1},'despike_f.nii.gz');
+    mcParams.mcFile             = fullfile(params.sessionDir,d{runNum},'despike_f.nii.gz');
+    mcParams.regFile            = fullfile(params.sessionDir,d{1},'despike_f.nii.gz');
 else
-    mcParams.mcFile           = fullfile(params.sessionDir,d{runNum},'raw_f.nii.gz');
-    mcParams.regFile          = fullfile(params.sessionDir,d{1},'raw_f.nii.gz');
+    mcParams.mcFile             = fullfile(params.sessionDir,d{runNum},'raw_f.nii.gz');
+    mcParams.regFile            = fullfile(params.sessionDir,d{1},'raw_f.nii.gz');
 end
-mcParams.outFile              = fullfile(params.sessionDir,d{runNum},'rf.nii.gz');
-mcParams.outDir               = fullfile(params.sessionDir,d{runNum});
+mcParams.outFile                = fullfile(params.sessionDir,d{runNum},'rf.nii.gz');
+mcParams.outDir                 = fullfile(params.sessionDir,d{runNum});
 if isfield(params,'regFirst') && params.regFirst
-    mcParams.dstFile          = fullfile(params.sessionDir,d{runNum},'dstFile.nii.gz');
+    mcParams.dstFile            = fullfile(params.sessionDir,d{runNum},'dstFile.nii.gz');
     system(['fslroi ' mcParams.regFile ' ' mcParams.dstFile ' 0 1']);
 end
 if isfield(params,'topup') && params.topup
     if ~isempty(strfind(d{runNum},'_AP'))
-        mcParams.phaseFile    = fullfile(params.sessionDir,'SpinEchoFieldMap','SpinEchoFieldMap_AP_01.nii.gz');
-        mcParams.warpFile     = fullfile(params.sessionDir,'SpinEchoFieldMap','PhaseOneWarp.nii.gz');
+        mcParams.phaseFile      = fullfile(params.sessionDir,'SpinEchoFieldMap','SpinEchoFieldMap_AP_01.nii.gz');
+        mcParams.warpFile       = fullfile(params.sessionDir,'SpinEchoFieldMap','PhaseOneWarp.nii.gz');
     elseif ~isempty(strfind(d{runNum},'_PA'))
-        mcParams.phaseFile    = fullfile(params.sessionDir,'SpinEchoFieldMap','SpinEchoFieldMap_PA_01.nii.gz');
-        mcParams.warpFile     = fullfile(params.sessionDir,'SpinEchoFieldMap','PhaseTwoWarp.nii.gz');
+        mcParams.phaseFile      = fullfile(params.sessionDir,'SpinEchoFieldMap','SpinEchoFieldMap_PA_01.nii.gz');
+        mcParams.warpFile       = fullfile(params.sessionDir,'SpinEchoFieldMap','PhaseTwoWarp.nii.gz');
     else
         error('No warp direction found in bold directory name');
     end
