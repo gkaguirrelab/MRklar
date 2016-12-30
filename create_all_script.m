40adb582d0ce7e566c53f4ad86819579ccd1743b
Andrew Bock <bock.andrew@gmail.com>
2016-12-30 15:32:46 -0500

    [ENH] create_all_script, create_anatomical_script - updates for GE data, freesurfer processing

--- create_all_script.m
+++ create_all_script.m
@@ -17,12 +17,23 @@ fprintf(fid,['dicomDir=' params.dicomDir '\n']);
 fprintf(fid,['useMRIcron=' num2str(params.useMRIcron) '\n']);
 fprintf(fid,['isGE=' num2str(params.isGE) '\n\n']);
 matlab_string = '"';
+SUBJECTS_DIR = getenv('SUBJECTS_DIR');
 %% Add anatomical scripts
 if params.reconall % If a new subject, for which recon-all has not been run
     fprintf(fid,['matlab -nodisplay -nosplash -r ' ...
         '"sort_nifti(''$SESS'',''$dicomDir'',$useMRIcron,$isGE);"\n']);
     if params.isGE
-        fprintf(fid,'recon-all -i $SESS/MPRAGE/001/MPRAGE.nii.gz -s $SUBJ -all\n');
+        fprintf(fid,'recon-all -i $SESS/MPRAGE/001/MPRAGE.nii.gz -s $SUBJ -autorecon1\n');
+        fprintf(fid,['mri_convert ' ...
+            fullfile(SUBJECTS_DIR,params.subjectName,'mri','T1.mgz') ' ' ...
+            fullfile(SUBJECTS_DIR,params.subjectName,'mri','T1.nii.gz') '\n']);
+        fprintf(fid,['bet ' ...
+            fullfile(SUBJECTS_DIR,params.subjectName,'mri','T1.nii.gz') ' ' ...
+            fullfile(SUBJECTS_DIR,params.subjectName,'mri','brainmask.nii.gz') ' -f 0.35\n']);
+        fprintf(fid,['mri_convert ' ...
+            fullfile(SUBJECTS_DIR,params.subjectName,'mri','brainmask.nii.gz') ' ' ...
+            fullfile(SUBJECTS_DIR,params.subjectName,'mri','brainmask.mgz') '\n']);
+        fprintf(fid,'recon-all -s $SUBJ -autorecon2 -autorecon3\n');
     else
         fprintf(fid,'recon-all -i $SESS/MPRAGE/001/ACPC/MPRAGE.ACPC.nii.gz -s $SUBJ -all\n');
     end
